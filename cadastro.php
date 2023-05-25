8a7f48bf91ee5fa4fcc33c3a08b68245222877a5
KarlaR <karlaroberta@github.com>
2023-05-24 21:09:50 -0300

    adicao  do css

--- cadastro.php
+++ cadastro.php
@@ -1,1 +1,11 @@
-Os dados foram enviados!
+Respostas:
+
+1- e- Hyper Text Markup Language.
+
+2- e- Linguagem de marcação, ou seja, serve para estruturar informações e criar páginas web.
+
+3- a- É utilizado para fornecer uma descrição da imagem.
+
+4- d- html, head, title , body e p.
+
+5- d-  Para acrescentar título apresentado com o maior tamanho de texto.
